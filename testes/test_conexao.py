b552d8e4ac906fb3f4aaa587a7e6bf9451b114f2
Gabriel lima <gabrielveigalima@icloud.com>
2020-02-16 17:55:30 -0300

    escrevendo testes para classe de conexao com twitter

---
+++ testes/test_conexao.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import pytest
+
+from src.conexao import conexao_twitter
+
+
+MODULE_DIR = os.path.dirname(__file__)
+sys.path.append(os.path.join(MODULE_DIR, '../..'))
+
+def test_conexao_com_twitter():
+    assert conexao_twitter.ConexaoTwitter().conectar() != None
+
+def test_varivais_de_ambiente_para_conectar_ao_twitter():
+    assert os.getenv('CONSUMER_KEY') != None
+    assert os.getenv('CONSUMER_SECRET') != None
+    assert os.getenv('ACCESS_TOKEN') != None
+    assert os.getenv('ACCESS_TOKEN_SECRET') != None
